280df1d107046d53232448f3fbde871ffa88c950
Hunter Parks and Jun Fritz <jun@>
2017-05-04 12:00:14 -0700

    add entire page

---
+++ roman_numerals/README.md
@@ -0,0 +1,7 @@
+######If user enters 'not a number' return false
+
+######User enters value. We split the value. 'ParseInt' the value.
+
+######Determine length of array and categorize in 7 functions based on each roman numeral and one additional function for checking length of output number. IE.. LXXXX needs to be XC
+
+######Each function is a function that determines if a certain character is one of 7 roman numerals. First function is I, second is V, etc...
---
+++ roman_numerals/js/scripts.js
@@ -0,0 +1,258 @@
+var array = [];
+
+var m_convert = function(input){
+  if(input.toString()[0] === "1"){
+    input = input.replace('1', 'M');
+    array.push(input.substring(0 , 1));
+    // console.log(input.substring(1));
+    return input.substring(1);
+  }
+  else if(input.toString()[0] === "2"){
+    input = input.replace('2', 'MM');
+    array.push(input.substring(0 , 2));
+    // console.log(input.substring(2));
+    return input.substring(2);
+  }
+  else if(input.toString()[0] === "3"){
+    input = input.replace('3', 'MMM');
+    array.push(input.substring(0 , 3));
+    // console.log(input.substring(3));
+    return input.substring(3);
+  }
+};
+
+var d_convert = function(input){
+  if(input.toString()[0] === "9"){
+    input = input.replace('9', 'CM');
+    array.push(input.substring(0 , 2));
+    console.log(input.substring(2));
+    console.log(array);
+    return input.substring(2);
+  }
+  else if(input.toString()[0] === "8"){
+    input = input.replace('8', 'DCCC');
+    array.push(input.substring(0 , 4));
+    console.log(input.substring(4));
+    console.log(array);
+    return input.substring(4);
+  }
+  else if(input.toString()[0] === "7"){
+    input = input.replace('7', 'DCC');
+    array.push(input.substring(0 , 3));
+    console.log(input.substring(3));
+    console.log(array);
+    return input.substring(3);
+  }
+  else if(input.toString()[0] === "6"){
+    input = input.replace('6', 'DC');
+    array.push(input.substring(0 , 2));
+    console.log(input.substring(2));
+    console.log(array);
+    return input.substring(2);
+  }
+  else if(input.toString()[0] === "5"){
+    input = input.replace('5', 'D');
+    array.push(input.substring(0));
+    console.log(input.substring(1));
+    console.log(array);
+    return input.substring(1);
+  }
+  else if(input.toString()[0] === "4"){
+    input = input.replace('4', 'CD');
+    array.push(input.substring(0 , 2));
+    console.log(input.substring(2));
+    console.log(array);
+    return input.substring(2);
+  }
+  else if(input.toString()[0] === "3"){
+    input = input.replace('3', 'CCC');
+    array.push(input.substring(0 , 3));
+    console.log(input.substring(3));
+    console.log(array);
+    return input.substring(3);
+  }
+  else if(input.toString()[0] === "2"){
+    input = input.replace('2', 'CC');
+    array.push(input.substring(0 , 2));
+    console.log(input.substring(2));
+    console.log(array);
+    return input.substring(2);
+  }
+  else if(input.toString()[0] === "1"){
+    input = input.replace('1', 'C');
+    array.push(input.substring(0, 1));
+    console.log(input.substring(1));
+    console.log(array);
+    return input.substring(1);
+  }
+
+};
+var l_convert = function(input){
+  if(input.toString()[0] === "9"){
+    input = input.replace('9', 'XC');
+    array.push(input.substring(0 , 2));
+    console.log(input.substring(2));
+    console.log(array);
+    return input.substring(2);
+  }
+  else if(input.toString()[0] === "8"){
+    input = input.replace('8', 'LXXX');
+    array.push(input.substring(0 , 4));
+    console.log(input.substring(4));
+    console.log(array);
+    return input.substring(4);
+  }
+  else if(input.toString()[0] === "7"){
+    input = input.replace('7', 'LXX');
+    array.push(input.substring(0 , 3));
+    console.log(input.substring(3));
+    console.log(array);
+    return input.substring(3);
+  }
+  else if(input.toString()[0] === "6"){
+    input = input.replace('6', 'LX');
+    array.push(input.substring(0 , 2));
+    console.log(input.substring(2));
+    console.log(array);
+    return input.substring(2);
+  }
+  else if(input.toString()[0] === "5"){
+    input = input.replace('5', 'L');
+    array.push(input.substring(0));
+    console.log(input.substring(1));
+    console.log(array);
+    return input.substring(1);
+  }
+  else if(input.toString()[0] === "4"){
+    input = input.replace('4', 'XL');
+    array.push(input.substring(0 , 2));
+    console.log(input.substring(2));
+    console.log(array);
+    return input.substring(2);
+  }
+  else if(input.toString()[0] === "3"){
+    input = input.replace('3', 'XXX');
+    array.push(input.substring(0 , 3));
+    console.log(input.substring(3));
+    console.log(array);
+    return input.substring(3);
+  }
+  else if(input.toString()[0] === "2"){
+    input = input.replace('2', 'XX');
+    array.push(input.substring(0 , 2));
+    console.log(input.substring(2));
+    console.log(array);
+    return input.substring(2);
+  }
+  else if(input.toString()[0] === "1"){
+    input = input.replace('1', 'X');
+    array.push(input.substring(0));
+    console.log(input.substring(1));
+    console.log(array);
+    return input.substring(1);
+  }
+};
+
+var x_convert = function(input){
+  if(input.toString()[0] === "9"){
+    input = input.replace('9', 'IX');
+    array.push(input.substring(0 , 2));
+    console.log(input.substring(2));
+    console.log(array);
+    return input.substring(2);
+  }
+  else if(input.toString()[0] === "8"){
+    input = input.replace('8', 'VIII');
+    array.push(input.substring(0 , 4));
+    console.log(input.substring(4));
+    console.log(array);
+    return input.substring(4);
+  }
+  else if(input.toString()[0] === "7"){
+    input = input.replace('7', 'VII');
+    array.push(input.substring(0 , 3));
+    console.log(input.substring(3));
+    console.log(array);
+    return input.substring(3);
+  }
+  else if(input.toString()[0] === "6"){
+    input = input.replace('6', 'VI');
+    array.push(input.substring(0 , 2));
+    console.log(input.substring(2));
+    console.log(array);
+    return input.substring(2);
+  }
+  else if(input.toString()[0] === "5"){
+    input = input.replace('5', 'V');
+    array.push(input.substring(0));
+    console.log(input.substring(1));
+    console.log(array);
+    return input.substring(1);
+  }
+  else if(input.toString()[0] === "4"){
+    input = input.replace('4', 'IV');
+    array.push(input.substring(0 , 2));
+    console.log(input.substring(2));
+    console.log(array);
+    return input.substring(2);
+  }
+  else if(input.toString()[0] === "3"){
+    input = input.replace('3', 'III');
+    array.push(input.substring(0 , 3));
+    console.log(input.substring(3));
+    console.log(array);
+    return input.substring(3);
+  }
+  else if(input.toString()[0] === "2"){
+    input = input.replace('2', 'II');
+    array.push(input.substring(0 , 2));
+    console.log(input.substring(2));
+    console.log(array);
+    return input.substring(2);
+  }
+  else if(input.toString()[0] === "1"){
+    input = input.replace('1', 'I');
+    array.push(input.substring(0));
+    console.log(input.substring(1));
+    console.log(array);
+    return input.substring(1);
+  }
+};
+
+var filter = function(input){
+
+};
+
+$(document).ready(function(){
+  $("form#mainform").submit(function(event){
+    event.preventDefault();
+    var input = $("input#numerals").val();
+    // input = input.split("");
+    input = input.replace(/[^0-9.]/g, "");
+    console.log(input);
+
+    if(input.length === 4){
+      // d_convert(m_convert(input));
+      var total = x_convert(l_convert(d_convert(m_convert(input))));
+      total = array.join("");
+      console.log(total);
+    }
+    else if(input.length === 3){
+      var total = x_convert(l_convert(d_convert(input)));
+      total = array.join("");
+      console.log(total);
+    }
+    else if(input.length === 2){
+      var total = x_convert(l_convert(input));
+      total = array.join("");
+      console.log(total);
+    }
+    else if(input.length === 1){
+      var total = x_convert(input);
+      total = array.join("");
+      console.log(total);
+    }
+
+
+  });
+});
